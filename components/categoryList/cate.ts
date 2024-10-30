d3953211de29893d88fae39567043b7a5f63f2a5
Hungnhatit <nhatpro204@gmail.com>
2024-10-30 20:04:15 +0700

    cate items data

---
+++ components/categoryList/cate.ts
@@ -0,0 +1,8 @@
+export const cates = [
+  { name: "style", style: "style", imgSrc: "/assets/icons/footwear.png" },
+  { name: "fashion", style: "fashion", imgSrc: "/assets/icons/footwear.png" },
+  { name: "food", style: "food", imgSrc: "/assets/icons/footwear.png" },
+  { name: "travel", style: "travel", imgSrc: "/assets/icons/footwear.png" },
+  { name: "culture", style: "culture", imgSrc: "/assets/icons/footwear.png" },
+  { name: "coding", style: "coding", imgSrc: "/assets/icons/footwear.png" },
+]
