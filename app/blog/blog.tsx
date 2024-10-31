654257384556da3cd9b955ad56f9e30f4476205b
Hungnhatit <nhatpro204@gmail.com>
2024-10-31 20:03:09 +0700

    blog page

---
+++ app/blog/blog.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import styles from './blog.module.css';
+
+
+const Blog = () => {
+  return (
+    <div className={styles.container}>
+      <h1 className={styles.title}>Styles blog</h1>
+      <div className=''>
+        <div></div>
+      </div>
+
+    </div>
+  )
+}
+
+export default Blog
