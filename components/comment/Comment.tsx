fb05e8f2f0ce44e29fcf4d98709785bce26b1e56
Hungnhatit <nhatpro204@gmail.com>
2024-11-03 15:31:28 +0700

    update component

--- components/comment/Comment.tsx
+++ components/comment/Comment.tsx
@@ -1,8 +1,45 @@
-import React from 'react'
+import React from 'react';
+import styles from './comment.module.css';
+import Link from 'next/link';
+import Image from 'next/image';
 
 const Comment = () => {
+  const status = 'authenticated';
+
+
   return (
-    <div>Comment</div>
+    <div className={styles.container}>
+      <h1 className={styles.title}>Comments</h1>
+      {status === 'authenticated'
+        ? (
+          <div className={styles.write}>
+            <textarea placeholder='Leave a comment ...' className={styles.input} />
+            <button className={styles.button}>Send</button>
+          </div>
+        )
+        : (
+          <Link href='/login'>Login to write a comment</Link>
+        )}
+      <div className={styles.comments}>
+        <div className={styles.comment}>
+          <div className={styles.user}>
+            <div className='w-[50px] h-[50px] relative'>
+              <Image src='/assets/imgs/landscape.jpg' alt='' fill className={styles.image} />
+            </div>
+            <div className={styles.userInfo}>
+              <span className={styles.username}>Hungnhatit</span>
+              <span className={styles.date}>03.11.2024</span>
+            </div>
+          </div>
+
+          <p className={styles.desc}>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione incidunt excepturi quo nostrum rem consequuntur maiores nulla neque natus animi autem blanditiis, facilis mollitia libero sapiente error voluptate at culpa.
+          </p>
+
+        </div>
+
+      </div>
+    </div>
   )
 }
 
