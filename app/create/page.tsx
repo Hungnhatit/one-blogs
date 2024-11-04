2e1ab8980f4f5266f535b05f14c634767d171c61
Hungnhatit <nhatpro204@gmail.com>
2024-11-04 16:12:17 +0700

    develop create post page

---
+++ app/create/page.tsx
@@ -0,0 +1,50 @@
+'use client'
+import React, { useState } from 'react';
+import styles from './create.module.css';
+
+import Image from 'next/image';
+import { CiCirclePlus, CiImageOn } from 'react-icons/ci';
+import { GoLinkExternal } from 'react-icons/go';
+import { FaPhotoVideo } from 'react-icons/fa';
+
+// Text editor
+import ReactQuill from 'react-quill';
+import 'react-quill/dist/quill.snow.css';
+
+const CreatePost = () => {
+  const [open, setOpen] = useState(false);
+  const [value, setValue] = useState('');
+
+  return (
+    <div className={styles.container}>
+      <input type="text" placeholder='Your post title...' className={styles.input}/>
+      <div className={styles.editor}>
+        <button className={styles.button} onClick={() => setOpen(!open)}>
+          <CiCirclePlus size={20} />
+        </button>
+        {open && (
+          <div className={styles.add}>
+            <button className={styles.addButton}>
+              <CiImageOn size={20} />
+            </button>
+            <button className={styles.addButton}>
+              <GoLinkExternal size={20} />
+            </button>
+            <button className={styles.addButton}>
+              <FaPhotoVideo size={20} />
+            </button>
+          </div>
+        )}
+        <ReactQuill
+          theme='bubble'
+          value={value}
+          onChange={setValue}
+          placeholder={`Tell your story...`}
+          className={`${styles.textArea} border`}></ReactQuill>
+      </div>
+      <button className={styles.publish}>Post</button>
+    </div>
+  )
+}
+
+export default CreatePost
