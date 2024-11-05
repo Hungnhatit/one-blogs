3b5db21b0c5b6d1b8e3e889d4d5f635d340747d2
Hungnhatit <nhatpro204@gmail.com>
2024-11-05 13:13:08 +0700

    ...

--- components/categoryList/cate.ts
+++
@@ -1,8 +0,0 @@
-export const cates = [
-  { name: "style", bgColor: "bg-[#57c4ff31]", imgSrc: "/assets/icons/footwear.png" },
-  { name: "fashion", bgColor: "bg-[#da85c731]", imgSrc: "/assets/icons/fashion.jpg" },
-  { name: "food", bgColor: "bg-[#7fb88133]", imgSrc: "/assets/icons/food.jpg" },
-  { name: "travel", bgColor: "bg-[#ff795736]", imgSrc: "/assets/icons/travel.jpg" },
-  { name: "culture", bgColor: "bg-[#ffb04f45]", imgSrc: "/assets/icons/culture.jpg" },
-  { name: "coding", bgColor: "bg-[#5e4fff31]", imgSrc: "/assets/icons/coding.avif" },
-]
