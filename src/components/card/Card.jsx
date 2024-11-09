b7ccd7de063757ffed28a7dce3a1d69190dabf42
Hungnhatit <nhatpro204@gmail.com>
2024-11-09 20:47:21 +0700

    chore: update UI

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -3,28 +3,28 @@ import styles from "./card.module.css";
 import Image from 'next/image';
 import Link from 'next/link';
 
-const Card = ({key, post}) => {
+const Card = ({ key, post }) => {
   return (
-    <div className={styles.container}>
+    <div key={key} className={styles.container}>
       <div className={styles.imgContainer}>
-        <Image
+        {post.img && <Image
           src={post.img}
           alt={post.catSlug}
           fill
-          className={styles.image} />
+          className={styles.image} />}
       </div>
       <div className={styles.textContainer}>
         <div className={styles.detail}>
           <span className={styles.date}>{post.createdAt.substring(0, 10)} - </span>
           <span className={styles.category}>{post.catSlug}</span>
         </div>
-        <Link href="" className='text-[28px] font-semibold'>
+        <Link href={`/post/${post.slug}`} className='text-[28px] font-semibold'>
           <h1>{post.title}</h1>
         </Link>
         <p className={styles.desc}>
-          {post.desc}
+          {post.desc.substring(0, 60)}
         </p>
-        <Link href="" className={styles.link}>Read More</Link>
+        <Link href={`/post/${post.slug}`} className={styles.link}>Read More</Link>
       </div>
     </div>
   )
